e024dd19b09111cb2995787b719e82ba03cfdd59
Jorge Lucas <jorge.lucasalm@gmail.com>
2021-10-18 22:35:02 -0300

    Adicionando a atividade

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        <Hello />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Olá mundo React</h1>
+        <p>Esse é o meu componente de classe :)</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
